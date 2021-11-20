cc60088c02d5c66caf9f092cdd68aa17f2099d1b
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-21 01:27:48 +0530

    Ability To Calculate Avg & Sum Of Basic Pay From Database

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
-
 import org.junit.Assert;
 import org.junit.Test;
 import java.sql.SQLException;
 import java.util.List;
-
+import java.util.Map;
 
 public class EmployeePayrollServiceTest {
+
     //Test Case To Check If Database Connection IS Properly Established Or Not
     @Test
     public void EmployeePayrollDataRetrievedDFromDB_MatchEmployeeCount() throws SQLException {
@@ -42,6 +42,22 @@ public class EmployeePayrollServiceTest {
     public void retrieveData_betweenRange() {
         EmployeePayrollService employeePayRollService=new EmployeePayrollService();
         employeePayRollService.retrieveData_inBetween_Range();
+    }
+
+    //Test Case To find Avg Basic pay
+    @Test
+    public void find_avg_BasicPay() {
+        EmployeePayrollService employeePayRollService=new EmployeePayrollService();
+        Map<String, Double> emp_Avg_salary = employeePayRollService.avg_BasePay_Base_on_gender();
+        System.out.println(emp_Avg_salary);
+    }
+
+    //Test Case To find Sum Of Basic pay
+    @Test
+    public void find_SumOf_basicPay() {
+        EmployeePayrollService employeePayRollService=new EmployeePayrollService();
+        Map<String, Double> emp_sum_salary = employeePayRollService.sumOf_BasicPay_Based_on_gender();
+        System.out.println(emp_sum_salary);
     }
 
 }
