cd6f8d12ee4cecefc9e706d3acda3f12e219f1ce
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-21 01:19:22 +0530

    Ability To Update Salary In Database

--- src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -5,20 +5,30 @@ import org.junit.Test;
 import java.sql.SQLException;
 import java.util.List;
 
-public class EmployeePayrollServiceTest {
 
+public class EmployeePayrollServiceTest {
+    //Test Case To Check If Database Connection IS Properly Established Or Not
     @Test
     public void EmployeePayrollDataRetrievedDFromDB_MatchEmployeeCount() throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         databaseConnection.getConnection();
     }
 
+    //Test Case To Check Whether Data Is Properly Retrieved Or Not From Database
     @Test
-    public void retrieveDataFromDB()
-    {
-        EmployeePayrollService employeePayRollService=new EmployeePayrollService();
-        List<EmployeeData> employee = employeePayRollService.retrieveData();
+    public void retrieveDataFromDB() {
+        EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+        List<EmployeeData> employee = employeePayrollService.retrieveData();
         Assert.assertEquals(5, employee.size());
+    }
+
+    //Test Case To Check Whether Salary Is Updated Or Not In Database
+    @Test
+    public void updateSalaryFromDB() {
+        EmployeePayrollService employeePayrollService=new EmployeePayrollService();
+        List<EmployeeData> employee = employeePayrollService.retrieveData();
+        employeePayrollService.updateSalary();
+
     }
 
 }
